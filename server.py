62e7e0e87dfbf43e72aaeac7e06924e9dce8435b
Alex Krantz <alex@alexkrantz.com>
2018-08-24 18:51:23 -0700

    Remove command line config

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser
 from configparser import ConfigParser, NoSectionError, NoOptionError
 from eventlet import monkey_patch
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request, jsonify
 from flask_socketio import SocketIO
 from os import urandom
 
@@ -12,51 +12,41 @@ socketio = SocketIO(app)
 data = {}
 
 parser = ArgumentParser()
-parser.add_argument("--config", help="config file to load from. NOTE: the config file takes prescience over arguments")
-parser.add_argument("--host", help="host to run the server on", default="127.0.0.1")
-parser.add_argument("--port", help="port to run the server on", default=5000)
-parser.add_argument("--debug", help="run the server in debug mode", action="store_true")
+parser.add_argument("config", help="config file to load from. NOTE: the config file takes prescience over arguments")
 args = parser.parse_args()
+config = ConfigParser()
+config.read(args.config)
 
-if args.config:
-    config = ConfigParser()
-    config.read(args.config)
+if "server" not in config.sections():
+    raise NoSectionError("Unable to find section 'server'")
+host = config["server"].get("host")
+port = config["server"].getint("port")
+debug = config["server"].getboolean("debug")
 
-    if "server" not in config.sections():
-        raise NoSectionError("Unable to find section 'server'")
-    host = config["server"].get("host")
-    port = config["server"].getint("port")
-    debug = config["server"].getboolean("debug")
-
-    globals = [s for s in config.sections() if s != "server"]
-    if not globals:
-        data["globals"] = None
-    else:
-        data["globals"] = {}
-
-    for s in globals:
-        keys = [k for k in config[s].keys() if "-" not in k]
-        if s != "unorganized":
-            data["globals"][s] = {}
-        for key in keys:
-            try:
-                t = eval(config[s][key + "-type"])
-                if s != "unorganized":
-                    data["globals"][s][key] = t(config[s][key])
-                else:
-                    data["globals"][key] = t(config[s][key])
-            except KeyError:
-                raise NoOptionError(key+"-type", "Argument type required for '{0}' in section '{1}'".format(key, s))
-
+globals = [s for s in config.sections() if s != "server"]
+if not globals:
+    data["globals"] = None
 else:
-    host = args.host
-    port = args.port
-    debug = args.debug
+    data["globals"] = {}
+
+for s in globals:
+    keys = [k for k in config[s].keys() if "-" not in k]
+    if s != "unorganized":
+        data["globals"][s] = {}
+    for key in keys:
+        try:
+            t = eval(config[s][key + "-type"])
+            if s != "unorganized":
+                data["globals"][s][key] = t(config[s][key])
+            else:
+                data["globals"][key] = t(config[s][key])
+        except KeyError:
+            raise NoOptionError(key+"-type", "Argument type required for '{0}' in section '{1}'".format(key, s))
 
 
 @app.route('/')
 def index():
-    return render_template(args.template)
+    return render_template("index.html")
 
 
 @socketio.on("data")
@@ -79,5 +69,4 @@ def on_disconnect():
 
 
 if __name__ == '__main__':
-    # socketio.run(app, host=args.host, port=args.port, debug=args.debug)
-    pass
+    socketio.run(app, host=host, port=port, debug=debug)
